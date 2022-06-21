c58eb930e4c842e89cf905c48adc6d04d8db36cc
Vadim <support@zavodil.ru>
2022-06-21 23:27:46 +0300

    brrr stats

---
+++ marketcap.js
@@ -0,0 +1,55 @@
+const {getConfig} = require('./near');
+const nearAPI = require('near-api-js');
+
+const config = getConfig(process.env.NODE_ENV || 'production');
+const brrrLockedHolders = ['lockup.burrow.near', 'burrow.sputnik-dao.near'];
+let brrrToken = 'token.burrow.near';
+// 100 billions
+const maxSupply = Math.pow(10, 9);
+
+const getTokenPrice = async (tokenId) => {
+    return fetch("https://indexer.ref-finance.net/list-token-price")
+        .then(res => res.json())
+        .then(json => json[tokenId].price || 0)
+        .catch(err => {
+            console.error(err);
+            return 0;
+        });
+}
+
+const updateMarketcap = async () => {
+    let marketCap = {
+        lockedBalances: [],
+        circulatingSupply: 0,
+        lastUpdate: 0
+    }
+
+    let tokenPrice = await getTokenPrice("token.burrow.near");
+    const near = await nearAPI.connect(config);
+
+    const lockedBalances = await Promise.all(
+        brrrLockedHolders.map(async (address) => {
+            const account = await near.account(address);
+            const ft_balance = await account.viewFunction(brrrToken, 'ft_balance_of', {account_id: address})
+            const parsedBalance = Number(ft_balance)/Math.pow(10,18);
+            return {
+                address,
+                balance: parsedBalance,
+                value: parsedBalance * tokenPrice
+            };
+        })
+    )
+    const sumLocked = lockedBalances.reduce((acc, value) => acc+value.balance, 0);
+    const circulatingSupply = maxSupply-sumLocked;
+    if(!isNaN(circulatingSupply)) {
+        marketCap.lockedBalances = lockedBalances;
+        marketCap.circulatingSupply = circulatingSupply;
+        marketCap.lastUpdate = new Date().getTime();
+    }
+
+    return(marketCap);
+}
+
+updateMarketcap().then((marketCap)=> {
+    console.log(marketCap)
+});
---
+++ near.js
@@ -0,0 +1,28 @@
+function getConfig(env) {
+    switch (env) {
+        case 'production':
+        case 'mainnet':
+            return {
+                networkId: 'mainnet',
+                nodeUrl: 'https://rpc.mainnet.near.org',
+                headers: { },
+                walletUrl: 'https://wallet.near.org',
+                helperUrl: 'https://helper.mainnet.near.org',
+                explorerUrl: 'https://explorer.mainnet.near.org',
+            }
+        case 'development':
+        case 'testnet':
+            return {
+                networkId: 'testnet',
+                nodeUrl: 'https://rpc.testnet.near.org',
+                headers: { },
+                walletUrl: 'https://wallet.testnet.near.org',
+                helperUrl: 'https://helper.testnet.near.org',
+                explorerUrl: 'https://explorer.testnet.near.org',
+            }
+        default:
+            throw Error(`Unconfigured environment '${env}'. Can be configured in src/config.js.`)
+    }
+}
+
+module.exports = {getConfig}
